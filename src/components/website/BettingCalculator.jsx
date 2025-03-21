6ec3114f1706b970df43cf2ab7679342c97bfe5f
divi2001 <deepashok20@gmail.com>
2025-03-21 12:30:06 +0530

    Uploading recent unkwown changes

--- src/components/website/BettingCalculator.jsx
+++ src/components/website/BettingCalculator.jsx
@@ -78,7 +78,7 @@ function BettingCalculator() {
     };
 
     try {
-      const response = await axios.post('http://16.16.10.60:5000/stbetting', payload, {
+      const response = await axios.post('http://localhost:5000/stbetting', payload, {
         headers: { 'Content-Type': 'application/json' }
       });
       // Expecting the response data to have a "results" array.
