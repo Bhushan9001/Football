e2a1dcd21f37aed2dbf1de032a0e6971c044991a
divi2001 <deepashok20@gmail.com>
2025-03-26 09:35:32 +0530

    Changed the origin of the prediction model

--- src/components/website/PredictionModel.jsx
+++ src/components/website/PredictionModel.jsx
@@ -32,7 +32,7 @@ const PredictionModel = () => {
     });
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('http://45.119.47.81:5000/predict', {
         method: 'POST',
         body: form
       });
